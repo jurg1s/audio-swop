34ab2330096cd9d007374a68616dae1b97e34e2c
jurgis <jurgis.bernatonis@gmail.com>
2024-11-04 21:20:02 +0200

    something works

--- src/setup.sh
+++ src/setup.sh
@@ -16,11 +16,5 @@ pip3 install -r /usr/local/bin/audio_swop/requirements.txt
 sudo cp /usr/local/bin/audio_swop/audio_swop.desktop /usr/share/applications/
 sudo chmod 644 /usr/share/applications/audio_swop.desktop
 
-# Ensure the icon is in the correct location
-sudo cp /usr/local/bin/audio_swop/icon.png /usr/local/bin/audio_swop/
-sudo chmod 644 /usr/local/bin/audio_swop/icon.png
-
-# Copy GIF
-sudo cp /usr/local/bin/audio_swop/spinner.gif /usr/local/bin/audio_swop/
 
 echo "Setup completed successfully."
